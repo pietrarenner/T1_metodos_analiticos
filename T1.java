ac44b1c63d3b592660bbf66822d6644840851e9a
Deborah <dcitrinf@gmail.com>
2025-04-26 18:13:29 -0300

    finalizado com json

--- T1.java
+++ T1.java
@@ -16,59 +16,117 @@ public class T1 {
     private static List<Evento> escalonador;
     private static double tempoGlobal;
     private static double ultimoTempo;
-    
-    public static void main(String[] args) {
-        System.out.println("Simulacao G/G/1/5");
-        HashMap<Integer, Double> map1 = new HashMap<>();
-        map1.put(1, 0.8);
-        map1.put(2, 0.2);
-        
-        HashMap<Integer, Double> map2 = new HashMap<>();
-        map2.put(-1, 0.2); // vem do json
-        map2.put(0, 0.3); // vem do json
-        map2.put(2, 0.5); // vem do json
-        
-        HashMap<Integer, Double> map3 = new HashMap<>();
-        map3.put(-1, 0.3); // vem do json
-        map3.put(1, 0.7); // vem do json
+    private static int numIteracoes;
 
-        Fila fila1 = new Fila(1, 100001, 2, 4, 1, 2, map1, 0); // vem do json
-        Fila fila2 = new Fila(2, 5, 0, 0, 4, 8, map2, 1); // vem do json
-        Fila fila3 = new Fila(2, 10, 0, 0, 5, 15, map3, 2); // vem do json
-        listaDeFilas.add(fila1);
-        listaDeFilas.add(fila2);
-        listaDeFilas.add(fila3);
-        simular();
+    public static void main(String[] args) {
+        ParametrosSimulador jsonReader = new ParametrosSimulador();
+        Map<String, Object> jsonContent = jsonReader.getJsonValues();
+
+        numIteracoes = (int) jsonContent.get("rndnumbersPerSeed");
+
+        LinkedHashMap<String, Object> probabilities = (LinkedHashMap<String, Object>) jsonContent.get("probabilities");
+        ArrayList<HashMap<Integer, Double>> probabilidadesTodasFilas = new ArrayList<>();
+        Map<String, Object> queues = (Map<String, Object>) jsonContent.get("queues");
+
+        for (Map.Entry<String, Object> entry : probabilities.entrySet()) {
+            HashMap<Integer, Double> probabilidadesDaFila = new HashMap<>();
+
+            Map<String, Object> probData = (Map<String, Object>) entry.getValue();
+
+            for (Map.Entry<String, Object> probEntry : probData.entrySet()) {
+                int proxLugar = Integer.parseInt(probEntry.getKey());
+                double probability = (double) probEntry.getValue();
+                probabilidadesDaFila.put(proxLugar, probability);
+            }
+
+            probabilidadesTodasFilas.add(probabilidadesDaFila);
+        }
+
+        for (Map.Entry<String, Object> entry : queues.entrySet()) {
+            String queueName = entry.getKey();
+            Map<String, Object> queueData = (Map<String, Object>) entry.getValue();
+
+            if (queueName.equals("Q1")) {
+                int index = (int) queueData.get("index");
+                int server = (int) queueData.get("servers");
+                int capacity = (int) queueData.get("capacity");
+
+                double minArrival = (double) queueData.get("minArrival");
+                double maxArrival = (double) queueData.get("maxArrival");
+
+                double minService = (double) queueData.get("minService");
+                double maxService = (double) queueData.get("maxService");
+
+                listaDeFilas.add(new Fila(server, capacity, minArrival, maxArrival,
+                        minService, maxService, probabilidadesTodasFilas.get(index), index));
+            }
+
+            else {
+                int index = (int) queueData.get("index");
+                int server = (int) queueData.get("servers");
+                int capacity = (int) queueData.get("capacity");
+
+                double minService = (double) queueData.get("minService");
+                double maxService = (double) queueData.get("maxService");
+
+                listaDeFilas.add(new Fila(server, capacity, 0, 0, minService, maxService, probabilidadesTodasFilas.get(index), index));
+            }
+        }
+
+        System.out.println("Simulacao G/G/1/5");
+        // HashMap<Integer, Double> map1 = new HashMap<>();
+        // map1.put(1, 0.8);
+        // map1.put(2, 0.2);
+
+        // HashMap<Integer, Double> map2 = new HashMap<>();
+        // map2.put(-1, 0.2); // vem do json
+        // map2.put(0, 0.3); // vem do json
+        // map2.put(2, 0.5); // vem do json
+
+        // HashMap<Integer, Double> map3 = new HashMap<>();
+        // map2.put(-1, 0.3); // vem do json
+        // map2.put(1, 0.7); // vem do json
+
+        // Fila fila1 = new Fila(1, 100001, 2, 4, 1, 2, map1, 0); // vem do json
+        // Fila fila2 = new Fila(2, 5, 0, 0, 4, 8, map2, 1); // vem do json
+        // Fila fila3 = new Fila(2, 10, 0, 0, 5, 15, map3, 2); // vem do json
+        // listaDeFilas.add(fila1);
+        // listaDeFilas.add(fila2);
+        // listaDeFilas.add(fila3);
+        Map<String, Double> chegada = (Map<String, Double>) jsonContent.get("arrivals");
+        double primeiraChegada = (double) chegada.get("0");
+        simular(primeiraChegada);
 
         // minArrival = 0 significa q a fila nao recebe de fora
     }
-    
-    private static void simular() {
+
+    private static void simular(double primeiraChegada) {
         escalonador = new ArrayList<>();
-        
-        escalonador.add(new Evento(tipo_evento.CHEGADA, 2, 0)); // vem do json
-        
-        for (int i = 0; i < 100000; i++) {
+
+        escalonador.add(new Evento(tipo_evento.CHEGADA, primeiraChegada, 0)); // vem do json
+
+        for (int i = 0; i < numIteracoes; i++) {
             escalonador.sort(Comparator.comparingDouble(e -> e.tempo));
             Evento evento = escalonador.remove(0);
-            
+
             tempoGlobal = evento.tempo;
             atualizarTempoEstadosFila();
 
-            // System.out.printf("evento: %s no tempo %.2f\n", evento.tipo ? "saida" : "chegada", evento.tempo);
+            // System.out.printf("evento: %s no tempo %.2f\n", evento.tipo ? "saida" :
+            // "chegada", evento.tempo);
             // System.out.printf("tam fila: %d\n", fila.size());
-            
+
             if (evento.tipo == tipo_evento.CHEGADA) {
                 chegada(listaDeFilas.get(evento.fila));
-            } else if(evento.tipo == tipo_evento.SAIDA) {
-                saida(listaDeFilas.get(evento.fila)); 
-            } else if(evento.tipo == tipo_evento.PASSAGEM) {
+            } else if (evento.tipo == tipo_evento.SAIDA) {
+                saida(listaDeFilas.get(evento.fila));
+            } else if (evento.tipo == tipo_evento.PASSAGEM) {
                 passagem(listaDeFilas.get(evento.fila));
             }
         }
         reportarResultados();
     }
-    
+
     private static void chegada(Fila filaDestino) { // no nosso caso sempre a fila 1
         // System.out.printf("chegada no tempo %.2f\n", tempoGlobal);
         if (filaDestino.getCustomers() < filaDestino.getCapacity()) {
@@ -80,11 +138,12 @@ public class T1 {
                     sum += entry.getValue();
                     if (prob < sum) {
                         if (entry.getKey() != -1) {
-                            double tempoPassagem = tempoGlobal + calculaTempo(filaDestino.getMinService(), filaDestino.getMaxService());
+                            double tempoPassagem = tempoGlobal
+                                    + calculaTempo(filaDestino.getMinService(), filaDestino.getMaxService());
                             escalonador.add(new Evento(tipo_evento.PASSAGEM, tempoPassagem, filaDestino.getIndex()));
-                        }
-                        else {
-                            double tempoSaida = tempoGlobal + calculaTempo(filaDestino.getMinService(), filaDestino.getMaxService());
+                        } else {
+                            double tempoSaida = tempoGlobal
+                                    + calculaTempo(filaDestino.getMinService(), filaDestino.getMaxService());
                             escalonador.add(new Evento(tipo_evento.SAIDA, tempoSaida, filaDestino.getIndex()));
                         }
                         break;
@@ -96,7 +155,8 @@ public class T1 {
             // System.out.println("cliente perdido");
         }
         double tempoProxChegada = tempoGlobal + calculaTempo(filaDestino.getMinArrival(), filaDestino.getMaxArrival());
-        escalonador.add(new Evento(tipo_evento.CHEGADA, tempoProxChegada, 0));  // TODO: pegar o indice do json no ARRIVALS
+        escalonador.add(new Evento(tipo_evento.CHEGADA, tempoProxChegada, 0)); // TODO: pegar o indice do json no
+                                                                               // ARRIVALS
         // System.out.printf("agendou proxima chegada para %.2f\n", tempoProxChegada);
     }
 
@@ -105,16 +165,18 @@ public class T1 {
         double sum = 0.0;
         double prob = NextRandom();
 
-        if (filaOrigem.getCustomers() >= filaOrigem.getServer()) { // 5 pessoas ainda não foram atendidas e temos 2 servidores 
+        if (filaOrigem.getCustomers() >= filaOrigem.getServer()) { // 5 pessoas ainda não foram atendidas e temos 2
+                                                                   // servidores
             for (Map.Entry<Integer, Double> entry : filaOrigem.getFilas().entrySet()) {
                 sum += entry.getValue();
                 if (prob < sum) {
                     if (entry.getKey() != -1) {
-                        double tempoPassagem = tempoGlobal + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
+                        double tempoPassagem = tempoGlobal
+                                + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
                         escalonador.add(new Evento(tipo_evento.PASSAGEM, tempoPassagem, filaOrigem.getIndex()));
-                    }
-                    else {
-                        double tempoSaida = tempoGlobal + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
+                    } else {
+                        double tempoSaida = tempoGlobal
+                                + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
                         escalonador.add(new Evento(tipo_evento.SAIDA, tempoSaida, filaOrigem.getIndex()));
                     }
                     break;
@@ -135,7 +197,8 @@ public class T1 {
                 sum += entry.getValue();
                 if (prob < sum) {
                     if (entry.getKey() != -1) {
-                        double tempoPassagem = tempoGlobal + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
+                        double tempoPassagem = tempoGlobal
+                                + calculaTempo(filaOrigem.getMinService(), filaOrigem.getMaxService());
                         escalonador.add(new Evento(tipo_evento.PASSAGEM, tempoPassagem, filaOrigem.getIndex()));
                         filaDestino = listaDeFilas.get(entry.getKey()); // fila seguinte
                     }
@@ -174,11 +237,11 @@ public class T1 {
             }
         }
     }
-    
+
     private static double calculaTempo(double a, double b) {
         return a + ((b - a) * NextRandom());
     }
-    
+
     private static double NextRandom() {
         anterior = ((a * anterior) + c) % M;
         return anterior / M;
@@ -188,7 +251,7 @@ public class T1 {
         double delta = tempoGlobal - ultimoTempo;
         // System.out.println("delta: " + delta);
 
-        for(Fila fila : listaDeFilas) {
+        for (Fila fila : listaDeFilas) {
             int ultimoTamFila = fila.getCustomers();
 
             if (ultimoTamFila >= 0 && ultimoTamFila <= fila.getCapacity()) {
@@ -201,7 +264,7 @@ public class T1 {
 
     private static void reportarResultados() {
         System.out.println("Tempo Global da Simulacao: " + tempoGlobal);
-        
+
         int totalPerdidos = 0;
         int j = 1;
         for (Fila fila : listaDeFilas) {
@@ -209,8 +272,8 @@ public class T1 {
             j++;
             totalPerdidos += fila.getLoss();
         }
-        // System.out.println("Clientes Totais Perdidos: " + totalPerdidos);
-    
+        System.out.println("Clientes Totais Perdidos: " + totalPerdidos);
+
         for (int idx = 0; idx < listaDeFilas.size(); idx++) {
             Fila fila = listaDeFilas.get(idx);
             System.out.println("Distribuicao de Probabilidades dos Estados da Fila " + (idx + 1) + ":");
@@ -226,7 +289,7 @@ class Evento {
     tipo_evento tipo;
     double tempo;
     int fila;
-    
+
     public Evento(tipo_evento tipo, double tempo, int fila) {
         this.tipo = tipo;
         this.tempo = tempo;
